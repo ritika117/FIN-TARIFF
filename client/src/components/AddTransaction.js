ae4a6dc23eb23f57daadda2d8671f6d17bb2bd69
ritika <rituritz11@gmail.com>
2022-07-15 23:56:58 +0530

    commit

--- client/src/components/AddTransaction.js
+++ client/src/components/AddTransaction.js
@@ -26,12 +26,12 @@ const AddTransaction = () => {
             <h3>Add new transaction</h3>
             <form onSubmit = {onSubmit}>
                 <div className = "form-control">
-                    <label htmlFor = "text">Text</label>
-                    <input type = "text" value = {text} onChange = {e => setText(e.target.value)} placeholder = "Enter text..." />
+                    <label htmlFor = "text">Description</label>
+                    <input type = "text" value = {text} onChange = {e => setText(e.target.value)} placeholder = "Enter description" />
                 </div>
                 <div className = "form-control">
-                    <label htmlFor = "amount">Amount <br/> (negative - expense, positive - income)</label>
-                    <input type = "number" value = {amount} onChange = {e => setAmount(e.target.value)} placeholder = "Enter amount..." />
+                    <label htmlFor = "amount">Amount <br/> (Add ' - ' for expense)</label>
+                    <input type = "number" value = {amount} onChange = {e => setAmount(e.target.value)} placeholder = "Enter amount" />
                 </div>
                 <button className = "btn">Add transaction</button>
             </form>
